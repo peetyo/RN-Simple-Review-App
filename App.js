4b91ab9d90c562d0590a2f3c27a5dd1971730a84
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-19 15:32:53 +0200

    Simple View, Text, Button and State

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('shaun');
+  const [person, setPerson] = useState({name: 'mario', age: 40});
+
+  const clickHandler = () => {
+    setName('petar');
+    setPerson({name: 'luigi', age: 45});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+  <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update name" onPress={clickHandler}/>
+      </View>
     </View>
   );
 }
@@ -16,4 +28,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20,
+  }
 });
