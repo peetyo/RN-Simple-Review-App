6b7a6a9dbe6d67706814c3724e739a95d388ba3c
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-22 20:52:05 +0200

    added navigation buttons

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) {
+
+  const pressHandler = () => {
+    // navigation.pop();
+    navigation.goBack();
+  }
 
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Review Details Screen</Text>
+      <Button title='Pop review details screen' onPress={pressHandler}/>
     </View>
   )
 }
