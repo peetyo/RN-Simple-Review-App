b606c27e6d465bd931a4629998ddc66dd0138bcc
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-22 21:28:16 +0200

    customized header a bit

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -7,11 +7,25 @@ import ReviewDetails from '../screens/ReviewDetails';
 const Stack = createStackNavigator()
 
 export default function Navigator() {
+
+  const generalOptions = {
+    headerStyle: { backgroundColor: '#eee', height: 80},
+    headerTintColor: '#444'
+  }
+
+  const homeOptions = {
+    // headerStyle: { backgroundColor: 'pink'}
+  }
+  
+  const reviewDetailsOptions = {
+    // headerStyle: { backgroundColor: 'pink'}
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='ReviewDetails' component={ReviewDetails} />
+      <Stack.Navigator initialRouteName='Home' screenOptions={generalOptions}>
+        <Stack.Screen name='Home' component={Home} options={homeOptions}/>
+        <Stack.Screen name='ReviewDetails' component={ReviewDetails} options={reviewDetailsOptions} />
       </Stack.Navigator>
     </NavigationContainer>
   );
