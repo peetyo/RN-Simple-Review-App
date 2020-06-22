0e11ba3e28fe572ce677d667e02858723efa455d
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-22 21:11:02 +0200

    pass data when navigating

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
+
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+    { title: 'Gotta Catch Them All (Again)', rating: 4, body: 'lorem ipsum', key: '2'},
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
+  ]);
 
   const pressHandler = () => {
     navigation.navigate('ReviewDetails');
@@ -11,8 +17,14 @@ export default function Home({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title='Go to review details' onPress={pressHandler}/>
+      <FlatList
+        data={reviews}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={()=> navigation.navigate('ReviewDetails', item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({navigation}) {
+export default function ReviewDetails({navigation, route}) {
 
-  const pressHandler = () => {
-    // navigation.pop();
-    navigation.goBack();
-  }
+  const { title, body, rating } = route.params
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Review Details Screen</Text>
-      <Button title='Pop review details screen' onPress={pressHandler}/>
+      <Text style={globalStyles.titleText}>{title}</Text>
+      <Text style={globalStyles.titleText}>{body}</Text>
+      <Text style={globalStyles.titleText}>{rating}</Text>
     </View>
   )
 }
