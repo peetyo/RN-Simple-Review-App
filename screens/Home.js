3fccd453fa2e48505166a526ad86d0b98f590e76
Petar <petar.bozhidarov.todorov@gmail.com>
2020-06-26 15:42:14 +0200

    Created a modal and added icons for trigger

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../styles/global';
 import Card  from '../shared/card';
 
 export default function Home({ navigation }) {
 
+  const [modalOpen,setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
     { title: 'Gotta Catch Them All (Again)', rating: 4, body: 'lorem ipsum', key: '2'},
@@ -18,6 +20,26 @@ export default function Home({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
+
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name='close'
+            size={24}
+            style={{...styles.modalToggle, ...styles.modalClose}}
+            onPress={()=> setModalOpen(false)}
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name='add'
+        size={24}
+        style={styles.modalToggle}
+        onPress={()=> setModalOpen(true)}
+      />
+
       <FlatList
         data={reviews}
         renderItem={({item}) => (
@@ -33,5 +55,19 @@ export default function Home({ navigation }) {
 }
 
 const styles = StyleSheet.create({
- 
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0
+  },
+  modalContent: {
+    flex: 1,
+  }
 })
